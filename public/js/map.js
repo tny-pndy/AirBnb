801df82e927736a3fb0697d8c821f510fa997e35
tny-pndy <129523868+tny-pndy@users.noreply.github.com>
2024-08-12 18:34:22 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -2,12 +2,17 @@
   mapboxgl.accessToken =mapToken ;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        style:'mapbox://styles/mapbox/streets-v9',
-      zoom: 9 // starting zoom
+        style:"mapbox://styles/mapbox/streets-v12",
+        center: listing.geometry.coordinates, 
+        // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        
+        zoom: 9 // starting zoom
     });
     const marker = new mapboxgl.Marker({color:'red'})
     .setLngLat(listing.geometry.coordinates)//Listing.geometry.coordinates
-    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.title}</h4><p>Exact location provided after booking</p>`))
-    .setMaxWidth("300px")
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(
+      `<h4>${listing.location}</h4><p>Exact location provided after booking</p>`
+    )
+  )
+    
     .addTo(map);
